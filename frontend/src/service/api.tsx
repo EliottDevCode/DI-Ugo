df9c8825a185aeefa4baa2e4ef9bc1b05847641d
eliottdev3 <eliott.dev.contact@gmail.com>
2025-05-01 18:53:41 +0200

    fix error on front with docker

---
+++ frontend/src/service/api.tsx
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+export interface Customer {
+  id: number
+  title: string
+  lastname: string
+  firstname: string
+  postalCode: number
+  city: string
+  email: string
+}
+
+export interface Order {
+  id: number
+  product: string
+  quantity: number
+  price: number
+  currency: string
+  date: string
+  customer: Customer
+}
+
+export const customerApi = {
+  getAll: () => api.get<Customer[]>('/customers'),
+  getById: (id: number) => api.get<Customer>(`/customers/${id}`),
+  create: (data: Omit<Customer, 'id'>) => api.post<Customer>('/customers', data),
+  update: (id: number, data: Partial<Customer>) => api.put<Customer>(`/customers/${id}`, data),
+  delete: (id: number) => api.delete(`/customers/${id}`),
+}
+
+export const orderApi = {
+  getAll: () => api.get<Order[]>('/orders'),
+  getById: (id: number) => api.get<Order>(`/orders/${id}`),
+  create: (data: Omit<Order, 'id'>) => api.post<Order>('/orders', data),
+  update: (id: number, data: Partial<Order>) => api.put<Order>(`/orders/${id}`, data),
+  delete: (id: number) => api.delete(`/orders/${id}`),
+}
